047cc84a8e9ea931331ac53bf064ed3950ece8e8
Walter Rodrigues <metal.walter@gmail.com>
2023-07-11 10:41:39 -0300

    Criando o Form para Add o funcionário

--- ASPNETMVCCRUD/Models/AddEmployeeViewModel.cs
+++ ASPNETMVCCRUD/Models/AddEmployeeViewModel.cs
@@ -1,8 +1,7 @@
-﻿namespace ASPNETMVCCRUD.Models.Domain
+﻿namespace ASPNETMVCCRUD.Models
 {
-    public class Employee
+    public class AddEmployeeViewModel
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public long Salary { get; set; }
--- ASPNETMVCCRUD/Program.cs
+++ ASPNETMVCCRUD/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MVCDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("MVCConnectionStrings")));
 
 var app = builder.Build();
 
